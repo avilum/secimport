127144ed1eb44916caf230f34428625eac7a9b0e
Avi Lumelsky <avilumelsky@Avis-MBP.lan>
2022-07-07 15:34:15 +0300

    Reformetted examples

--- examples/production.py
+++ examples/production.py
@@ -1,9 +1,9 @@
-from secimport import secure_import 
+from secimport import secure_import
 
-example = secure_import('example', allow_shells=False)
-print('Example imported successfully')
+example = secure_import("example", allow_shells=False)
+print("Example imported successfully")
 
- # Expected output:
+# Expected output:
 """
 (root) sh-3.2#  export PYTHONPATH=$(pwd)/src:$(pwd)/examples:$(pwd):$PYTHONPATH
 (root) sh-3.2#  python examples/production.py 
@@ -11,4 +11,3 @@ Successfully compiled dtrace profile:  /tmp/.secimport/sandbox_example.d
 (running dtrace supervisor):  sudo  dtrace -q -s /tmp/.secimport/sandbox_example.d -p 6484 -o /tmp/.secimport/sandbox_example.log &2>/dev/null
 Killed: 9
 """
-
