7a0ae39d34b3ede1fba61d658f0c392ff59ad637
Avi Lumelsky <avilume@gmail.com>
2022-11-13 15:22:05 +0200

    Added examples volume to docker run.sh

--- docker/docker/run_sandbox.sh
+++ docker/docker/run_sandbox.sh
@@ -5,7 +5,8 @@
 #            You can remove this argument.
 
 echo "Starting secimport sandbox with bpftrace backend, the sandbox should kill the python process..."
-bpftrace -c "/workspace/Python-3.10.0/python -c __import__('os').system('ps')" -o sandbox.log sandbox.bt --unsafe || echo "The process was killed, as expected."
+bpftrace -c "/workspace/Python-3.10.0/python -c __import__('os').system('ps')" -o sandbox.log sandbox.bt --unsafe
+echo "The process was killed, as expected."
 echo "The sandbox bpftrace code is at sandbox.bt"
 echo "The sandbox log is at sandbox.log"
 # tail -n 20 sandbox.log
--- docker/run.sh
+++ docker/run.sh
@@ -11,4 +11,4 @@ fi
 KERNEL_VERSION=`docker run --rm -it alpine uname -r | cut -d'-' -f1`
 
 cd .. # back to repo root dir
-docker run --rm --name=secimport --privileged -v "$(pwd)/src/secimport":"/workspace/secimport/" -it secimport:${KERNEL_VERSION}
+docker run --rm --name=secimport --privileged -v "$(pwd)/src/secimport":"/workspace/secimport/" -v "$(pwd)/examples":"/workspace/examples/" -it secimport:${KERNEL_VERSION}
