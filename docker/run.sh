e897b61c2f0cf5e37b91d95b94e5f681740a2538
Avi Lumelsky <avilume@gmail.com>
2023-04-30 01:47:44 +0300

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -8,8 +8,6 @@ else
     exit 1;
 fi
 
-KERNEL_VERSION=`docker run --rm -it alpine uname -r | cut -d'-' -f1`
-
 cd .. # back to repo root dir
 # Add your code dir with -v
-docker run --rm --name=secimport -p 8000:8000 --privileged -v "$(pwd)/secimport":"/workspace/secimport/" -v "$(pwd)/examples":"/workspace/examples/" -it secimport:${KERNEL_VERSION}
+docker run --rm --name=secimport -p 8000:8000 --privileged -v "$(pwd)/secimport":"/workspace/secimport/" -v "$(pwd)/examples":"/workspace/examples/" -it secimport
